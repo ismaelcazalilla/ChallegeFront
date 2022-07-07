f1ae9f82f6b8d493e90466816d3ba28b1b081ccb
Ismael Cazalilla <ismael.cazalilla@gmail.com>
2022-07-07 18:17:36 +0200

    test e2e: content home page

---
+++ cypress/e2e/home_page_spec.cy.js
@@ -0,0 +1,18 @@
+
+describe('Home', () => {
+
+  beforeEach(() => {
+    cy.visit('localhost:3000/');
+  });
+  
+  it('should have a header with h1', () => {
+    cy.get('.header').should('be.visible');
+    cy.get('.header h1').should('be.visible');
+  });
+
+  it('should have page with top and bottom container', () => {
+    cy.get('.page .container-top').should('be.visible');
+    cy.get('.page .container-bottom').should('be.visible');
+  });
+
+});
