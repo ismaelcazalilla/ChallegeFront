05011885531cc16a6baa1d3653ae24e4a316ce50
Ismael Cazalilla <ismael.cazalilla@gmail.com>
2022-07-11 12:50:04 +0200

    feature: created recommended state and move recommended slice to Ts

---
+++ src/recommended/redux/RecommendedState.ts
@@ -0,0 +1,5 @@
+import Recipe from "../../recipe/model/Recipe";
+
+export interface RecommendedState {
+  recommendedList: Recipe[]
+}
--- src/recommended/redux/recommendedSlice.ts
+++ src/recommended/redux/recommendedSlice.ts
@@ -1,14 +1,18 @@
 import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+import Recipe from '../../recipe/model/Recipe';
 import RecipeMapper from '../../recipe/service/ReciveMapper';
 import RecommendedService from '../service/RecommendedService';
+import { RecommendedState } from './RecommendedState';
+
+export const initialRecommendedState: RecommendedState = {
+  recommendedList: []
+}
 
 export const recommendedSlice = createSlice({
   name: 'recommended',
-  initialState: {
-    recommendedList: []
-  },
+  initialState: initialRecommendedState,
   reducers: {},
-  extraReducers: (builder) => {
+  extraReducers: (builder): void => {
     builder.addCase(getAllRecommended.fulfilled, (state, action) => {
       state.recommendedList = action.payload;
     });
@@ -17,11 +21,11 @@ export const recommendedSlice = createSlice({
 
 export const getAllRecommended = createAsyncThunk(
   'recommended/getAll',
-  async () => {
+  async (): Promise<Recipe[]> => {
     const response = await RecommendedService.getAll();
     return response.data.map(recipeData => RecipeMapper.map(recipeData));
   }
 );
 
-export const selectAllRecommended = (state) => state.recommendedList;
+export const selectAllRecommended = (state: RecommendedState) => state.recommendedList;
 export default recommendedSlice.reducer;
