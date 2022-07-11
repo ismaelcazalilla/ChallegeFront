b9d5f749dfd3e31d42f5ef2356e49842e75ad040
Ismael Cazalilla <ismael.cazalilla@gmail.com>
2022-07-11 12:38:29 +0200

    fix: removed multiple calls  to get all recommended in recommended list component

--- src/recommended/components/list/RecommendedList.tsx
+++ src/recommended/components/list/RecommendedList.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import { useEffect } from 'react';
 import { Link } from "react-router-dom";
 
 import { useAppSelector, useAppDispatch } from '../../../redux/hooks';
@@ -10,7 +10,7 @@ const RecommendedList = () => {
 
   useEffect(() => {
     dispatch(getAllRecommended())
-  });
+  }, [dispatch]);
  
   return (
     <div>
