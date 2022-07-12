7efa80e52c6fe7e93d8e949fc32be0eb85ae2718
Ismael Cazalilla <ismael.cazalilla@gmail.com>
2022-07-12 13:08:15 +0200

    refactor: recipe list to styled c.

--- src/recipe/components/recipe/RecipeItem.tsx
+++ src/recipe/components/recipe/RecipeItem.tsx
@@ -2,6 +2,52 @@ import { Link } from "react-router-dom";
 import { useAppDispatch } from "../../../redux/hooks";
 import Recipe from '../../model/Recipe';
 import { selectRecipe } from "../../redux/RecipeSlice";
+import styled from 'styled-components';
+
+const RecipeContainer = styled.section`
+  display: flex;
+  height: 300px;
+  border-radius: 20px;
+  overflow: hidden;
+  background-color: #fff;
+  box-shadow: #bbb 0px 0px 5px;
+`;
+
+const RecipeImg = styled.img`
+  height: 100%;
+`;
+
+const RecipeInfoContainer = styled.div`
+  flex: 1;
+  padding: 20px 30px;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+`;
+
+const RecipeCategory = styled.p`
+  font-size: 20px;
+  font-weight: bold;
+  color: #666;
+`;
+
+const RecipeName = styled.h3`
+  font-size: 25px;
+  font-weight: bold;
+  color: #333;
+  margin-top: 10px;
+`;
+
+const RecipeStuff = styled.div`
+  display: flex;
+  gap: 20px;
+  margin-top: 10px;
+`;
+
+const RecipeStuffInfo = styled.p`
+  font-size: 18px;
+  color: #666;
+`;
 
 const RecipeItem = (props: {item: Recipe }) => {
   const { item } = props;
@@ -10,21 +56,20 @@ const RecipeItem = (props: {item: Recipe }) => {
   const navigateToRecipeDetail = () => dispatch(selectRecipe(item));
 
   return (
-    <div className="recipe">
-      <img
-        className="recipe-image"
+    <RecipeContainer>
+      <RecipeImg        
         src={item.photo || process.env.REACT_APP_RECIPE_PHOTO_PLACEHOLDER}
         alt={item.name}
       />
-      <div className="recipe-info">
+      <RecipeInfoContainer>
         <div>
-          <p className="recipe-category">{item.categoryName}</p>
-          <h3 className="recipe-name">{item.name}</h3>
-          <div className="recipe-stuff">
-            <p className="recipe-time">{item.duration} minutes</p>
-            <p className="recipe-complexity">{item.complexity}</p>
-            <p className="recipe-people">{item.people} people</p>
-          </div>
+          <RecipeCategory>{item.categoryName}</RecipeCategory>
+          <RecipeName>{item.name}</RecipeName>
+          <RecipeStuff>
+            <RecipeStuffInfo>{item.duration} minutes</RecipeStuffInfo>
+            <RecipeStuffInfo>{item.complexity}</RecipeStuffInfo>
+            <RecipeStuffInfo>{item.people} people</RecipeStuffInfo>
+          </RecipeStuff>
         </div>
         <Link
           className="recipe-button"
@@ -33,8 +78,8 @@ const RecipeItem = (props: {item: Recipe }) => {
           onClick={navigateToRecipeDetail}>
           See more
         </Link>
-      </div>
-    </div>
+      </RecipeInfoContainer>
+    </RecipeContainer>
   );
 
 }
