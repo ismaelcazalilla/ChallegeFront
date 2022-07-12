cdd2de43e8691441969bc067a07dcae55864f2a1
Ismael Cazalilla <ismael.cazalilla@gmail.com>
2022-07-12 13:46:27 +0200

    refactor: recommended components to styled c.

--- src/recommended/components/list/RecommendedList.tsx
+++ src/recommended/components/list/RecommendedList.tsx
@@ -3,6 +3,27 @@ import { useAppDispatch, useAppSelector } from '../../../redux/hooks';
 import { getAllRecommended, selectAllRecommended } from '../../redux/RecommendedSlice';
 import { RecommendedItem } from '../recommended/RecommendedItem';
 import Recipe from '../../../recipe/model/Recipe';
+import styled from 'styled-components';
+
+const RecommendedListContainer = styled.section`
+  padding: 20px 100px;
+`;
+
+const RecommendedContainer = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  gap: 20px;
+  height: 200px;
+  align-items: stretch;
+`;
+
+const Title = styled.h2`
+  font-size: 30px;
+  font-weight: bold;
+  color: #333;
+  margin-bottom: 20px;
+`;
 
 const RecommendedList = () => {
   const list: Recipe[] = useAppSelector(selectAllRecommended);
@@ -13,16 +34,15 @@ const RecommendedList = () => {
   }, [dispatch]);
  
   return (
-    <div className="container-top">
+    <RecommendedListContainer>
       <div>
-        <h2 className="title">Recommended</h2>
-        <div className="recommendations">
-          {list.map((item: Recipe, index: number) =>
+        <Title>Recommended</Title>
+        <RecommendedContainer>{list.map((item: Recipe, index: number) =>
             <RecommendedItem item={item} index={index} key={`${item.id}_recommended`}/>
           )}
-        </div>
+        </RecommendedContainer>
       </div>
-    </div>
+    </RecommendedListContainer>
   )
 
 }
--- src/recommended/components/recommended/RecommendedItem.tsx
+++ src/recommended/components/recommended/RecommendedItem.tsx
@@ -1,12 +1,27 @@
 import { Link } from "react-router-dom";
 import Recipe from "../../../recipe/model/Recipe"
+import { useAppDispatch } from "../../../redux/hooks";
+import { selectRecipe } from "../../../recipe/redux/RecipeSlice";
+import styled from 'styled-components';
+
+const RecommendedImg = styled.img`
+  width: 100%;
+  height: 100%;
+  border-radius: 20px;
+`;
 
 export const RecommendedItem = (props: {item: Recipe, index: number }) => {
   const { item } = props;
+  const dispatch = useAppDispatch();
 
+  const navigateToRecipeDetail = () => dispatch(selectRecipe(item));
+  
   return (
-    <Link to="/detail" key={item.id}>
-      <img
+    <Link 
+      to={`/detail/${item.id}`}
+      key={item.id}
+      onClick={navigateToRecipeDetail}>
+      <RecommendedImg
         className="recommended-image"
         src={item.photo || process.env.REACT_APP_RECIPE_PHOTO_PLACEHOLDER}
         alt={item.name}
