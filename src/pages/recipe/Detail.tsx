e8eba197dbe114869e2163943b1df857145930f7
Ismael Cazalilla <ismael.cazalilla@gmail.com>
2022-07-12 14:37:42 +0200

    fix: removed unnecessary import

--- src/pages/recipe/Detail.tsx
+++ src/pages/recipe/Detail.tsx
@@ -1,6 +1,5 @@
 import RecipeDetail from "../../recipe/components/detail/RecipeDetail";
 import Header from "../shared/header/Header";
-import "./style.css";
 
 export default function Detail() {
   return (
