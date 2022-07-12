90bee5242451f9cf041985ae151bed13adbb68ff
Ismael Cazalilla <ismael.cazalilla@gmail.com>
2022-07-12 14:36:02 +0200

    refactor: moved recipe detail component to styled c.

--- src/recipe/components/detail/RecipeDetail.tsx
+++ src/recipe/components/detail/RecipeDetail.tsx
@@ -22,6 +22,56 @@ const RecipeImg = styled.img`
   border-radius: 20px;
 `;
 
+const RecipeInfo = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  flex: 1;
+`;
+
+const RecipeName = styled.h2`
+ {
+  font-size: 25px;
+  font-weight: bold;
+  color: #333;
+  margin-top: 10px;
+}`;
+
+const RecipeIngredientsContainer = styled.div`
+  margin-top: 40px;
+`;
+
+const RecipeIngredientsTitle = styled.h4`
+  font-size: 20px;
+  color: #333;
+  font-weight: bold;
+`;
+
+const RecipeIngredient = styled.p`
+  margin-top: 10px;
+  text-align: center;
+`;
+
+const RecipeCategory = styled.p`
+  font-size: 20px;
+  font-weight: bold;
+  color: #666;
+`;
+
+const RecipeDescription = styled.h4`
+  font-size: 30px;
+  font-weight: bold;
+  color: #333;
+  margin-bottom: 20px;
+`;
+
+const RecipeDescriptionText = styled.p`
+  font-size: 18px;
+  color: #666;
+  line-height: 1.5;
+  letter-spacing: 0.2px;
+`;
+
 const RecipeDetail = () => {
   const selected: Recipe = useAppSelector(selectSelectedRecipe);
 
@@ -32,21 +82,21 @@ const RecipeDetail = () => {
           src={selected.photo || process.env.REACT_APP_RECIPE_PHOTO_PLACEHOLDER}
           alt={selected.name}
         />
-        <div className="top-information">
-          <p className="recipe-detail-category">{selected.categoryName}</p>
-          <h2 className="recipe-detail-name">{selected.name}</h2>
+        <RecipeInfo>
+          <RecipeCategory>{selected.categoryName}</RecipeCategory>
+          <RecipeName>{selected.name}</RecipeName>
           <RecipeStuff item={selected} />
-          <div className="ingredients">
-            <h3>Ingredients</h3>
+          <RecipeIngredientsContainer>
+            <RecipeIngredientsTitle>Ingredients</RecipeIngredientsTitle>
             {selected.ingredients.splitIngredients().map((ingredient, index) => (
-              <p key={`${index}_ingredients`}>{ingredient}</p>
+              <RecipeIngredient key={`${index}_ingredients`}>{ingredient}</RecipeIngredient>
             ))}
-          </div>
-        </div>
+          </RecipeIngredientsContainer>
+        </RecipeInfo>
       </RecipeContainerFlex>
       <RecipeContainer>
-        <h4 className="description">Info</h4>
-        <p className="description-text">{selected.description}</p>
+        <RecipeDescription>Info</RecipeDescription>
+        <RecipeDescriptionText>{selected.description}</RecipeDescriptionText>
       </RecipeContainer>
     </Main>
   );
