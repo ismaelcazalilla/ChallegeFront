979024bc2feb90db39bc4c8063a0c0401be06eaf
Ismael Cazalilla <ismael.cazalilla@gmail.com>
2022-07-12 12:51:44 +0200

    refactor: added styled c. to header component

--- src/pages/shared/header/Header.tsx
+++ src/pages/shared/header/Header.tsx
@@ -1,13 +1,29 @@
 import { Link } from "react-router-dom"
+import styled from 'styled-components';
+
+const Nav = styled.nav`
+  background-color: #333;
+  box-shadow: #333 0px 0px 10px;
+  padding: 0 100px;
+  position: fixed;
+  width: 100%;
+  height: 50px;
+  display: flex;
+  align-items: center;
+`;
+
+const Title = styled.h1`
+  color: white;
+`;
 
 const Header = () => {
 
   return (
-    <nav className="header">
+    <Nav className="header">
       <Link to="/">
-        <h1>Food recipes</h1>
+        <Title>Food recipes</Title>
       </Link>
-    </nav>
+    </Nav>
   )
 }
 
